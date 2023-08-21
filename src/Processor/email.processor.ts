b139f02fd7cc96618bec1fc58d39f90caa66668b
Nana_Kwame <kemiwals93@gmail.com>
2023-08-21 13:39:51 +0000

    updated login

--- src/Processor/email.processor.ts
+++ src/Processor/email.processor.ts
@@ -43,3 +43,43 @@ export class EmailProcessor{
 
     }
 }
+
+
+@Processor('reset-password')
+export class PasswordProcessor {
+    private readonly logger = new Logger(EmailProcessor.name);
+    constructor (private mailService: MailerService){}
+    
+
+    @Process('reset_mail')
+    async handleTranscode(job: Job) {
+        this.logger.debug('Start transcoding...');
+        let details = job.data?.details;
+        // console.log(details?.email)
+        try {
+            await this.mailService.sendMail({
+                from: process.env.MAIL_FROM_ADDRESS,
+                to: details?.email,
+                subject: 'Reset Password',
+                template: 'Emailverification',
+                context: {
+                    email : details?.email,
+                    org_name: details?.org_name,
+                    link: details?.link,
+                    year: details?.year
+                }
+            }).then((r) => {
+                console.log(r, 'email is sent');
+                this.logger.debug('Transcoding completed');
+            }).catch((e) => {
+                console.log(e, 'error sending email');
+                this.logger.debug('Transcoding Failed');
+            })
+           
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+}
+
