f672042daab72afdc3a5a82a12d58465ac1188e5
amegavi08 <amegavifrancis@gmail.com>
2023-09-15 13:30:06 +0000

    changes

--- src/database/migrations/20230890180408-guest.js
+++ src/database/migrations/20230890180408-guest.js
@@ -70,8 +70,8 @@ module.exports = {
       },
       signOutTime: {
         type: Sequelize.TIME,
-        allowNull: true,
-        defaultValue: null
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       visitStatus: {
         type: Sequelize.ENUM('Signed In', 'Signed Out'),
