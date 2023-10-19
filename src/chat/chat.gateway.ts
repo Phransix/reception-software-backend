5e4d5332d3d58bf7a69a2471cbea4a91d28d9c26
Nana_Kwame <kemiwals93@gmail.com>
2023-10-19 16:58:40 +0000

    changes

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,65 +1,48 @@
-// import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
-// import { Server, Socket } from 'socket.io';
-
-// @WebSocketGateway()
-// export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-
-//   @WebSocketServer() server: Server;
-
-//  async handleConnection(client: Socket) {
-//     console.log(`Client connected: ${client.id}`);
-//     this.server.emit('message', `User ${client.id} connected.`);
-//   }
-
-//   handleDisconnect(client: Socket) {
-//     console.log(`Client disconnected: ${client.id}`);
-//     this.server.emit('message', `User ${client.id} disconnected.`);
-//   }
-
-//   handleMessage(client: Socket, message: string) {
-//     this.server.emit('message', `User ${client.id}: ${message}`);
-//   }
-// }
-
-
-
-import { Logger } from '@nestjs/common';
 import {
-    WebSocketGateway,
-    WebSocketServer,
-    SubscribeMessage,
-    OnGatewayConnection,
-    OnGatewayDisconnect,
-  } from '@nestjs/websockets';
-  import { Server, Socket } from 'socket.io';
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-  @WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
 
-  export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    @WebSocketServer() server: Server;
-    users: number = 0;
-
-  
-    async handleConnection() {
-      // A client has connected
-      this.users++;
-  
-      // Notify connected clients of current users
-      this.server.emit('message', `Users ${this.users} connected.`);
-    }
-  
-    async handleDisconnect() {
-      // A client has disconnected
-      this.users--;
-  
-      // Notify connected clients of current users
-      this.server.emit('message', `Users ${this.users} disconnected.`);
-    }
-  
-    @SubscribeMessage('chat')
-    async onChat(client: Socket, message: string) {
-      client.broadcast.emit('message', `chat: ${message}`);
-    //   return message
-    }
-
+  // Client Connection
+  async handleConnection(client: Socket) {
+    console.log(`client id: ${client?.id}`);
+    this.server.emit('message', `User ${client.id} connected.`);
+    console.log('connected');
   }
+
+  // Client Disconnection
+  handleDisconnect(client: Socket) {
+    console.log(`client id: ${client.id}`);
+    this.server.emit('message', `User ${client.id} disconnected.`);
+    console.log('disconnected');
+  }
+
+  // Chat Messgaes
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('new_notification', {
+     data:{
+      type: 'Sign In',
+      uniqueId: '15d86ff1-94ec-468b-9e47-3f4e666cc876',
+      name: 'John Doe',
+      time: '1:20 pm',
+     }
+    });
+    
+  }
+}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,116 +1,126 @@
-
-import {  BelongsTo, Column, DataType, ForeignKey,Model, Table } from "sequelize-typescript";
-import { LoginDTO } from "src/guard/auth/loginDTO";
-import { Organization } from "src/modules/organization/entities/organization.entity";
+import {
+  BeforeValidate,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { LoginDTO } from 'src/guard/auth/loginDTO';
+import { Organization } from 'src/modules/organization/entities/organization.entity';
 const { v4: uuidv4 } = require('uuid');
-import { Role } from "src/modules/role/role.enum";
+import { Role } from 'src/modules/role/role.enum';
+const { Sequelize, DataTypes } = require('sequelize');
 
 @Table({
   paranoid: true,
 })
 export class User extends Model<User> {
-
   static validateUser(loginDto: LoginDTO) {
     throw new Error('Method not implemented.');
   }
- 
+
   @Column({
     type: DataType.INTEGER,
     allowNull: false,
     autoIncrement: true,
     primaryKey: true,
-})
-id: number
+  })
+  id: number;
 
   @Column({
     defaultValue: uuidv4,
     type: DataType.UUID,
     allowNull: false,
-    unique: true
+    unique: true,
   })
-  userId: string
+  userId: string;
 
-
-    @ForeignKey(() => Organization)
-    @Column({
-      defaultValue: uuidv4,
-        type: DataType.STRING,
-        allowNull: false,
-        unique:true,
-        references: {
-          model: {
-            tableName: 'Organization',
-          },
-          key: 'organizationId',
-        },
-        onDelete: 'CASCADE',
-    })
-    organizationId: string;
-     @BelongsTo(() => Organization,{
-    foreignKey : 'organizationId',
-    targetKey : 'organizationId',
+  @ForeignKey(() => Organization)
+  @Column({
+    defaultValue: uuidv4,
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+    references: {
+      model: {
+        tableName: 'Organization',
+      },
+      key: 'organizationId',
+    },
+    onDelete: 'CASCADE',
+  })
+  organizationId: string;
+  @BelongsTo(() => Organization, {
+    foreignKey: 'organizationId',
+    targetKey: 'organizationId',
     as: 'Organization',
-     })
-     organization :Organization;
+  })
+  organization: Organization;
 
-
-  @Column({ type: 'enum', 
-  values: Object.values(Role), 
-  defaultValue: Role?.Receptionist 
-})
+  @Column({
+    type: 'enum',
+    values: Object.values(Role),
+    defaultValue: Role?.Receptionist,
+  })
   roleName: Role;
 
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  fullName: string;
 
-    @Column({
-        type: DataType.STRING,
-        allowNull: false,
-        unique: true
-    })
-    fullName: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+  })
+  email: string;
 
-    @Column({
-      type: DataType.STRING,
-        allowNull: false
-    })
-    email: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  phoneNumber: string;
 
-     @Column({
-      type: DataType.STRING,
-        allowNull: false
-    })
-    phoneNumber: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  profilePhoto: string;
 
-    @Column({
-      type: DataType.STRING,
-      allowNull: true
-     })
-     profilePhoto: string;
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: true,
+    defaultValue: false,
+  })
+  isLogin: boolean;
 
-     @Column({
-      type: DataType.BOOLEAN,
-      allowNull: true,
-      defaultValue: false
-    })
-    isLogin: boolean;
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  password: string;
 
-     @Column({
-      type: DataType.STRING,
-        allowNull: true
-    })
-    password: string;
+  @Column({
+    type: DataType.DATE,
+    allowNull: true,
+    defaultValue: null,
+  })
+  deletedAt: Date;
 
-    @Column({
-      type : DataType.DATE,
-      allowNull: true,
-      defaultValue: null
-    })
-    deletedAt: Date
+  role: Role[];
 
-   
-
-      role: Role[]
-   
-
- 
-
+  @BeforeValidate
+  static async validateUserInfo(instance: User, options: any): Promise<void> {
+    if (options.fields.includes('email', 'phoneNumber')) {
+      if (instance) {
+        throw new Error(`Account has been deactivated`);
+      }
+    }
+  }
 }
+
