ebd35fa376103e720c1fcab2a5e8e8dc1eaf6156
francis477 <francisteye477@gmail.com>
2023-11-17 16:13:08 +0000

    isojss

--- src/Processor/defaultPassword.processor.ts
+++ src/Processor/defaultPassword.processor.ts
@@ -17,13 +17,12 @@ export class defaultPaswordProcessor{
         this.logger.debug('Start transcoding...');
         let details = job.data?.details;
         // console.log(details?.email)
-        let dir_path = process.env.NODE_ENV == 'production' ? 'dist/mails/defaultPassword' : 'defaultPassword'
         try {
             await this.mailService.sendMail({
                 from: process.env.MAIL_FROM_ADDRESS,
                 to: details?.email,
                 subject: 'Default Password',
-                template: dir_path,
+                template: '/defaultPassword',
                 context: {
                     email : details?.email,
                     org_name: details?.org_name,
